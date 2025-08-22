64b93b425a10c3fc971d88dfd258df000339f79d
Maxim Golovlev <golovlev.maxim@gmail.com>
2025-08-22 16:05:36 +0300

    Initial Commit

---
+++ SimplyTip/SimplyTipApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimplyTipApp.swift
+//  SimplyTip
+//
+//  Created by Maxim Golovlev on 22.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct SimplyTipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
